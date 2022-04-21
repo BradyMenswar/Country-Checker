e1b58c97108f74ba4e331ae998e802736a5a6c6c
BradyMMenswar <bradymenswar@ufl.edu>
2022-04-21 15:56:10 -0400

    Added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
 import React from "react"
-
+import reactLogo from "../images/logo.png"
 export default function Navbar()
 {
-    return <h1>Navbar component</h1>
+    return (
+    
+        <nav>
+            <img src={reactLogo} className="nav--logo"/>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
 }
